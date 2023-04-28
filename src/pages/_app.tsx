44db43669ccada93ac2ea53981b3b00726455029
zhiyan114 <zhiyan114@zhiyan114.com>
2023-04-27 23:02:10 -0400

    Update some package

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,21 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
+import { Fragment } from 'react'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Fragment>
+    <Head>
+      <title>zhiyan114's Survey Response</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1"/>
+      <meta name="theme-color" content="#00FFFF" />
+      <meta property="og:type" content="website"/>
+      <meta property="og:url" content="https://ela.zhiyan114.com/"/>
+      <meta property="og:site_name" content="ELA Survey - zhiyan114"/>
+      <meta property="og:title" content="Home Page" key="title"/>
+      <meta property="og:description" content="Mini-Research Project for ELA Class" key="mainDesc"/>
+      <meta name="description" content="Mini-Research Project for ELA Class" key="altDesc"/>
+    </Head>
+    <Component {...pageProps} />
+  </Fragment>
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,38 +16,11 @@ export default function Home() {
       </Head>
       <main className={`${styles.main} ${inter.className}`}>
         <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>src/pages/index.tsx</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
-          </div>
+          <p>Test</p>
         </div>
 
         <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
+          <p>test 2</p>
         </div>
 
         <div className={styles.grid}>
