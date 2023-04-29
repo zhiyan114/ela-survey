a2dd2f0670a4ec6f620f3aae349b20de7ceeba09
zhiyan114 <zhiyan114@zhiyan114.com>
2023-04-28 23:51:37 -0400

    Fixed unescaped quote

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -7,7 +7,7 @@ export default class Error404 extends Component {
             <main className={`${styles.main}`}>
                 <div className={`${styles.center} ${styles.formBox}`}>
                     <h1>404 Error</h1>
-                    <p>I'm sorry, but the page you&#39;re looking for is not found, please check the link and try again.</p>
+                    <p>I&#39;m sorry, but the page you&#39;re looking for is not found, please check the link and try again.</p>
                 </div>
             </main>
         )
