7af0496af795e81fa0cfca98005076dfd0b34952
zhiyan114 <zhiyan114@zhiyan114.com>
2023-04-28 21:07:00 -0400

    Remove sentry release tag due to some issues

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -2,7 +2,6 @@ import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
   dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
-  release: process.env.NEXT_PUBLIC_SENTRY_RELEASE,
   integrations: [new Sentry.Replay()],
   beforeSend: (event, hint) => {
     if(!['critical', 'error', 'fatal'].find(el=> el === event.level)) return null; // Don't capture anything that isn't an error
--- sentry.edge.config.ts
+++ sentry.edge.config.ts
@@ -2,6 +2,5 @@ import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
   dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
-  release: process.env.NEXT_PUBLIC_SENTRY_RELEASE,
   tracesSampleRate: 0.5,
 });
